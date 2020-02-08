09340a77aafa7883f7ddc7bcf37d5ed2adf620db
Manoj Gangaraja <manoj.gangaraja@gmail.com>
2020-02-08 19:52:32 +0530

    added more operations
    added multiplication and division by 2
    
    Signed-off-by: Manoj Gangaraja <manoj.gangaraja@gmail.com>

--- test.py
+++ test.py
@@ -1,8 +1,15 @@
-
 print("enter the number")
-x=input(x)
-x=x/2
-return x
+x=input("Num: ")
+x=int(x)
+print("Enter Mul/Div for Multiplication/Division by 2")
 
+s =(input("Operation: \n"))
+if((s) == 'Mul'):
+    x = x * 2
+elif (s == 'Div'):
+    x = x / 2
+else:
+    print("opration not supported")
 
+print("Result is" + str(x))
 
